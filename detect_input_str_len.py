e15fa798418b785eff2312f79a0b61857a2a05a3
BetterBite <38665746+BetterBite@users.noreply.github.com>
2024-11-11 15:53:59 +0000

    functionality for all possible addresses for buffer overflow exploits

--- detect_input_str_len.py
+++ detect_input_str_len.py
@@ -1,3 +1,5 @@
+import re
+
 # disable the pagination line that shows when script is ran in other ways that "source script" within gdb
 # has no effect if ran in gdb itself
 gdb.execute("set pagination off")
@@ -7,12 +9,25 @@ for bp in gdb.breakpoints():
     bp.delete()
 
 # calls to library functions that are susceptible to a buffer overflow
-calls = ["strcpy@plt", "gets@plt"]
+calls = ["strcpy@plt", "strcpy", "gets@plt", "gets"]
+
+# find all possible addresses of the calls
+def find_all_addresses(call):
+    info = gdb.execute("info functions " + call, to_string=True)
+    addresses = re.findall(r"0x[0-9a-f]+", info)
+    addresses = list(set(addresses)) # remove duplicates
+    print("addresses found for", call, ":", addresses)
+    return addresses
 
 # for each of those calls add a potential breakpoint if it exists
 for call in calls:
     try:
-        bp = gdb.Breakpoint(call)
+        addresses = find_all_addresses(call)
+        print(type(addresses))
+        if (len(addresses) == 0):
+            continue
+        for address in addresses:
+            gdb.Breakpoint("*" + address)
     except gdb.error as e:
         pass
 
@@ -21,6 +36,10 @@ gdb.execute("r")
 
 # for each breakpoint, print bytes to overwrite return address
 for b in gdb.breakpoints():
+
+    # some breakpoints may not be reached by the time the program is finished, early exit
+    if gdb.selected_frame() is None:
+        break
 
     # ignore breakpoint is pending, i.e. did not find that call when made earlier
     if b.pending:
@@ -47,7 +66,7 @@ for b in gdb.breakpoints():
     buffer_address = (stack_bytes[3] << 24) | (stack_bytes[2] << 16) | (stack_bytes[1] << 8) | (stack_bytes[0])
 
     # get the end of the address where the eip is saved at by getting value of ebp and adding 4 for saved eip address and another 4 to get to the end of it
-    saved_iep_address_end = frame.read_register("ebp") + 8
+    saved_iep_address_end = frame.read_register("ebp") + 4
 
     # calculation and printing
     num_overwrite_bytes = int(saved_iep_address_end) - int(buffer_address)
@@ -56,4 +75,4 @@ for b in gdb.breakpoints():
 
     gdb.execute("c")
 
-
+gdb.execute("quit")
